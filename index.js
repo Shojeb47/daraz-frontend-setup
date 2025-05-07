b4a5c29f535e2e1f2b9c3ba6bfd87d074d9b130b
Shojeb47 <audiolibraryhotline@gmail.com>
2025-05-08 03:25:47 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Daraz Frontend (Next.js)</h1>
+      <p>Deployed on Render Successfully 🎉</p>
+    </div>
+  );
+}
